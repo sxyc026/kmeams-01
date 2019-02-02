c2fb96afdaeb02a64a02485bcf819cf19b394c33
Lee <1870547303@qq.com>
2019-02-02 18:20:28 +0800

    构建项目

---
+++ src/main/java/com/algorithm/kmeams01/Kmeams01Application.java
@@ -0,0 +1,14 @@
+package com.algorithm.kmeams01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kmeams01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Kmeams01Application.class, args);
+    }
+
+}
+
---
+++ src/test/java/com/algorithm/kmeams01/Kmeams01ApplicationTests.java
@@ -0,0 +1,17 @@
+package com.algorithm.kmeams01;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Kmeams01ApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
